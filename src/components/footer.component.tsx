f335ea5c008fd40b38ceededf8c47495a042fd52
Areksbi <36035819+Areksbi@users.noreply.github.com>
2025-02-21 18:31:39 +0000

    remove twitter temporary

--- src/components/footer.component.tsx
+++ src/components/footer.component.tsx
@@ -31,7 +31,7 @@ export const FooterComponent = () => (
             </a>
 
             {/* Twitter/X */}
-            <a
+            {/* <a
                 href="https://twitter.com/strangerssapp"
                 rel="noopener noreferrer"
                 className="text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-black p-1 rounded-full"
@@ -40,7 +40,7 @@ export const FooterComponent = () => (
                 <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                     <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
                 </svg>
-            </a>
+            </a> */}
 
             {/* TikTok */}
             <a
